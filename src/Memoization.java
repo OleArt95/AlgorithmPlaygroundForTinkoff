544beb489bbf31a1c7ca454b732fd96150a2e4d9
OlegArt95 <Wickodin@gmail.com>
2023-05-30 16:01:58 +0300

    Add memoization in fibonacci number calculating method

---
+++ src/Memoization.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Memoization {
+
+    public static void main(String[] args) {
+        int n = 10;
+        long[] mem = new long[n + 1];
+
+        Arrays.fill(mem, -1);
+
+        System.out.println(fibNaive(n, mem));
+    }
+
+    private static long fibNaive(int n, long[] mem) {
+
+        if (mem[n] != -1)
+            return mem[n];
+
+        if (n <= 1) {
+            return n;
+        }
+
+        long result = fibNaive(n - 1, mem) + fibNaive(n - 2, mem);
+        mem[n] = result;
+
+        return result;
+//        return fibNaive(n - 1, mem) + fibNaive(n - 2, mem);
+    }
+}
