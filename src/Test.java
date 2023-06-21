0bcc2c3cf8b07cdd5505e5dd6e2bdb57d474b127
OlegArt95 <Wickodin@gmail.com>
2023-06-21 13:16:35 +0300

    Add Test class

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,17 @@
-public class Test {
+class Test {
+
+    public String main(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String result = strs[0];
+
+        for (int i = 0; i < strs.length; i++) {
+            while (strs[i].indexOf(result) != 0) {
+                result = result.substring(0, result.length() - 1);
+                if (result.isEmpty()) return "";
+            }
+        }
+        return result;
+    }
 }
